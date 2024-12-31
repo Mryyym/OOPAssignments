9b01ae86ee3bbf65a10e24bbd7ef7ecbd41ddba8
MARIAM YAHIA <mariam0yehia@gmail.com>
2024-12-31 11:21:48 +0200

    regions

--- FirstAssignment/Program.cs
+++ FirstAssignment/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1.	Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
+            #endregion
+
+            #region 2.	Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+            #endregion
+
+            #region 3.	Create an enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+            #endregion
+
+            #region 4.	Assign the following Permissions (Read, write, Delete, Execute) in a form of Enum. ⮚	Create Variable from previous Enum to Add and Remove Permission from variable, check if specific Permission is existed inside variable
+
+            #endregion
+
+            #region 5.	Create an enum called "Colors" with the basic colors (Red, Green, Blue) as its members. Write a C# program that takes a color name as input from the user and displays a message indicating whether the input color is a primary color or not.
+            #endregion
+
+            #region 6.	Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+            #endregion
+
+            #region 7.	Create a struct called "Person" with properties "Name" and "Age". Write a C# prog
+            #endregion
+
+            #region 8.	ram that takes details of 3 persons as input from the user and displays the name and age of the oldest person
+            #endregion
+
         }
     }
 }
