e6136620a1abc0b44cf813875ec11f9edc6eecab
MARIAM YAHIA <mariam0yehia@gmail.com>
2025-01-11 16:45:31 +0200

    indexer

---
+++ SecondAssignment/PhoneBook.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondAssignment
+{
+    struct PhoneBook
+    {
+        string[] names;
+        long[] numbers;
+        int size;
+
+        public int Size
+        {
+            get { return size; }
+        }
+
+        //to get  person number by his namee
+        public long getPersonNum(string Name)
+        {
+            if(names is not null && numbers is not null)
+            {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (Name == names[i]) return numbers[i];
+                }
+            }
+           return 0;
+        }
+
+
+        //to set  person number by his namee
+
+        public void setPrsonNum(string Name, long Num)
+        {
+            if (names is not null && numbers is not null)
+            {
+                for (int i = 0; i < names.Length; i++)
+                {
+                    if (Name == names[i])
+                    {
+                        numbers[i] = Num;
+                        return;
+                    }
+                }
+            }
+        }
+
+
+        #region INDEXER
+
+        public long this[string Name]
+        {
+            get
+            {
+                if (names is not null && numbers is not null)
+                {
+                    for (int i = 0; i < names.Length; i++)
+                    {
+                        if (Name == names[i]) return numbers[i];
+                    }
+                }
+                return 0;
+            }
+            set
+            {
+                if (names is not null && numbers is not null)
+                {
+                    for (int i = 0; i < names.Length; i++)
+                    {
+                        if (Name == names[i])
+                        {
+                            numbers[i] = value;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public string this[int index]
+        {
+            get { return $"{index} - {names[index]} - {numbers[index]}"; }
+        }
+
+        #endregion
+        ///constructor
+        public PhoneBook(int _size)
+        {
+            size = _size;
+            names = new string[size];
+            numbers = new long[size];
+        }
+
+        public void AddPerson(int position, string name, long num)
+        {
+            {
+                if (names is not null && numbers is not null)
+                {
+                    if (position >= 0 && position < size)
+                    {
+                        names[position] = name;
+                        numbers[position] = num;
+                    }
+                }
+            }
+
+
+        } 
+        
+    }
+
+
+        
+}
---
+++ SecondAssignment/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace SecondAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01
+            #region with getter and settr
+            /*
+            PhoneBook Note = new PhoneBook(3);
+            Note.AddPerson(0, "Ahmed", 123);
+            Note.AddPerson(1, "Anas", 456);
+            Note.AddPerson(2, "Aya", 789);
+            #endregion
+            
+             
+          Console.WriteLine( Note.getPersonNum("Aya"));
+            Note.setPrsonNum("Aya",102030);
+            */
+            #endregion
+
+            #region INDEXER
+
+            PhoneBook Note = new PhoneBook(3);
+            Note.AddPerson(0, "Ahmed", 123);
+            Note.AddPerson(1, "Anas", 456);
+
+
+            /*
+             Note.AddPerson(2, "Aya", 789);
+            Note["Ahmed"] = 2025;
+            Console.WriteLine(Note["Ahmed"]);
+            */
+
+
+            //
+            // Console.WriteLine(Note[1]);
+            #endregion
+
+
+            #region MyRegion
+
+            #endregion
+
+            #endregion
+
+
+            #region Part02
+            #region Design and implement a Class for the employees in a company
+
+            #endregion
+
+
+            #region Develop a Class to represent the Hiring Date Data:
+
+            #endregion
+
+
+            #region Create an array of Employees with size three a DBA, Guest and the third one is security officer who have full permissions. (Employee [] EmpArr
+
+            #endregion
+
+
+            #region Sort the employees based on their hire date then Print the sorted array.
+
+            #endregion
+
+
+            #endregion
+        }
+    }
+}
